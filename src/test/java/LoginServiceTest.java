00e6f8d72c73780c04b877df66d6392c875cd1d0
Lucjan <biklucjan@gmail.com>
2015-06-01 17:53:13 +0200

    login service to verify

--- src/test/java/LoginServiceTest.java
+++ src/test/java/LoginServiceTest.java
@@ -14,16 +14,6 @@ import java.util.*;
  * Created by lucjan on 30.05.15.
  */
 public class LoginServiceTest {
-
-    @Mock
-    private UserDataBase userDataBase;
-
-    @Mock
-    private Encoder encoder;
-
-    @Mock
-    private LoginService loginService;
-
     private List<User> contentOfDatabase = new ArrayList<User>();
 
     @Before
@@ -41,9 +31,11 @@ public class LoginServiceTest {
         contentOfDatabase.add(new User(name1, surname1));
         contentOfDatabase.add(new User(name2, surname2));
         contentOfDatabase.add(new User(name3, surname3));
+    }
 
-
-        userDataBase = Mockito.mock(UserDataBase.class);
+    @Test
+    public void getUserFromDB() {
+        UserDataBase userDataBase = Mockito.mock(UserDataBase.class);
         Mockito.when(userDataBase.getUserByLoginAndPassword(Matchers.anyString(),Matchers.anyString())).thenAnswer(new Answer<User>() {
 
             @Override
@@ -61,7 +53,7 @@ public class LoginServiceTest {
         });
 
 
-        encoder = Mockito.mock(Encoder.class);
+        Encoder encoder = Mockito.mock(Encoder.class);
 
         Mockito.when(encoder.encode(Matchers.anyString())).thenAnswer(new Answer<String>() {
             @Override
@@ -71,47 +63,131 @@ public class LoginServiceTest {
                 return encodeBase64(text);
             }
         });
-    }
 
-//    LoginService tests
-    @Test
-    public void loginServiceSuccessTest() {
         LoginService loginService1 = new LoginService(userDataBase, encoder);
         Assert.assertTrue(loginService1.login("Jan", "Nowak"));
-//        Assert.assertTrue(userDataBase.isConnected());
     }
 
     @Test
     public void loginServiceFailureTestWithNameNull() {
+//        LoginService loginService1 = new LoginService(userDataBase, encoder);
+        UserDataBase userDataBase = Mockito.mock(UserDataBase.class);
+        Mockito.when(userDataBase.getUserByLoginAndPassword(Matchers.anyString(),Matchers.anyString())).thenAnswer(new Answer<User>() {
+
+            @Override
+            public User answer(InvocationOnMock invocationOnMock) throws Throwable {
+
+                String name = invocationOnMock.getArguments()[0].toString();
+                String surname = invocationOnMock.getArguments()[1].toString();
+
+                for (User user : contentOfDatabase) {
+                    if (user.getName().equals(name) && user.getSurname().equals(surname))
+                        return user;
+                }
+                return null;
+            }
+        });
+
+
+        Encoder encoder = Mockito.mock(Encoder.class);
+
+        Mockito.when(encoder.encode(Matchers.anyString())).thenAnswer(new Answer<String>() {
+            @Override
+            public String answer(InvocationOnMock invocationOnMock) throws Throwable {
+//                do kodowania zosalo uzyte kodowanie Base64 ktore jest juz zaimplementowane w Javie
+                String text = invocationOnMock.getArguments()[0].toString();
+                return encodeBase64(text);
+            }
+        });
+
         LoginService loginService1 = new LoginService(userDataBase, encoder);
         Assert.assertFalse(loginService1.login(null, "Nowak"));
     }
 
     @Test
     public void loginServiceFailureTestSurnameNull() {
+//        LoginService loginService1 = new LoginService(userDataBase, encoder);
+        UserDataBase userDataBase = Mockito.mock(UserDataBase.class);
+        Mockito.when(userDataBase.getUserByLoginAndPassword(Matchers.anyString(),Matchers.anyString())).thenAnswer(new Answer<User>() {
+
+            @Override
+            public User answer(InvocationOnMock invocationOnMock) throws Throwable {
+
+                String name = invocationOnMock.getArguments()[0].toString();
+                String surname = invocationOnMock.getArguments()[1].toString();
+
+                for (User user : contentOfDatabase) {
+                    if (user.getName().equals(name) && user.getSurname().equals(surname))
+                        return user;
+                }
+                return null;
+            }
+        });
+
+
+        Encoder encoder = Mockito.mock(Encoder.class);
+
+        Mockito.when(encoder.encode(Matchers.anyString())).thenAnswer(new Answer<String>() {
+            @Override
+            public String answer(InvocationOnMock invocationOnMock) throws Throwable {
+//                do kodowania zosalo uzyte kodowanie Base64 ktore jest juz zaimplementowane w Javie
+                String text = invocationOnMock.getArguments()[0].toString();
+                return encodeBase64(text);
+            }
+        });
+
         LoginService loginService1 = new LoginService(userDataBase, encoder);
         Assert.assertFalse(loginService1.login("Jan", null));
     }
 
     @Test
     public void loginServiceFailureTestWithBothNullParameters() {
+//        LoginService loginService1 = new LoginService(userDataBase, encoder);
+        UserDataBase userDataBase = Mockito.mock(UserDataBase.class);
+        Mockito.when(userDataBase.getUserByLoginAndPassword(Matchers.anyString(),Matchers.anyString())).thenAnswer(new Answer<User>() {
+
+            @Override
+            public User answer(InvocationOnMock invocationOnMock) throws Throwable {
+
+                String name = invocationOnMock.getArguments()[0].toString();
+                String surname = invocationOnMock.getArguments()[1].toString();
+
+                for (User user : contentOfDatabase) {
+                    if (user.getName().equals(name) && user.getSurname().equals(surname))
+                        return user;
+                }
+                return null;
+            }
+        });
+
+
+        Encoder encoder = Mockito.mock(Encoder.class);
+
+        Mockito.when(encoder.encode(Matchers.anyString())).thenAnswer(new Answer<String>() {
+            @Override
+            public String answer(InvocationOnMock invocationOnMock) throws Throwable {
+//                do kodowania zosalo uzyte kodowanie Base64 ktore jest juz zaimplementowane w Javie
+                String text = invocationOnMock.getArguments()[0].toString();
+                return encodeBase64(text);
+            }
+        });
+
         LoginService loginService1 = new LoginService(userDataBase, encoder);
         Assert.assertFalse(loginService1.login(null, null));
     }
-
 
 //    UserDataBase tests
 
     @Test(expected = RuntimeException.class)
     public void connectingFailureTest() {
-        userDataBase = Mockito.mock(UserDataBase.class);
+        UserDataBase userDataBase = Mockito.mock(UserDataBase.class);
         Mockito.doThrow(new RuntimeException()).when(userDataBase).connect();
         userDataBase.connect();
     }
 
     @Test
     public void connectingFailureTest2() {
-        userDataBase = Mockito.mock(UserDataBase.class);
+        UserDataBase userDataBase = Mockito.mock(UserDataBase.class);
         Mockito.doThrow(new RuntimeException("Cannot connect with database")).when(userDataBase).connect();
         try {
             userDataBase.connect();
@@ -123,7 +199,7 @@ public class LoginServiceTest {
 
     @Test
     public void connectingSuccessTest() {
-        userDataBase = Mockito.mock(UserDataBase.class);
+        UserDataBase userDataBase = Mockito.mock(UserDataBase.class);
         Mockito.when(userDataBase.isConnected()).thenAnswer(new Answer<Boolean>() {
             @Override
             public Boolean answer(InvocationOnMock invocationOnMock) throws Throwable {
@@ -136,6 +212,35 @@ public class LoginServiceTest {
 
     @Test
     public void getUserFromDataBaseWhenExists() {
+        UserDataBase userDataBase = Mockito.mock(UserDataBase.class);
+        Mockito.when(userDataBase.getUserByLoginAndPassword(Matchers.anyString(),Matchers.anyString())).thenAnswer(new Answer<User>() {
+
+            @Override
+            public User answer(InvocationOnMock invocationOnMock) throws Throwable {
+
+                String name = invocationOnMock.getArguments()[0].toString();
+                String surname = invocationOnMock.getArguments()[1].toString();
+
+                for (User user : contentOfDatabase) {
+                    if (user.getName().equals(name) && user.getSurname().equals(surname))
+                        return user;
+                }
+                return null;
+            }
+        });
+
+
+        Encoder encoder = Mockito.mock(Encoder.class);
+
+        Mockito.when(encoder.encode(Matchers.anyString())).thenAnswer(new Answer<String>() {
+            @Override
+            public String answer(InvocationOnMock invocationOnMock) throws Throwable {
+//                do kodowania zosalo uzyte kodowanie Base64 ktore jest juz zaimplementowane w Javie
+                String text = invocationOnMock.getArguments()[0].toString();
+                return encodeBase64(text);
+            }
+        });
+
         User userFromDB = userDataBase.getUserByLoginAndPassword("Adam", encodeBase64("Kowalski"));
         Assert.assertNotNull(userFromDB);
         Assert.assertEquals("Adam", userFromDB.getName());
@@ -144,6 +249,34 @@ public class LoginServiceTest {
 
     @Test
     public void getUserFromDataBaseWhenDoesntExists() {
+        UserDataBase userDataBase = Mockito.mock(UserDataBase.class);
+        Mockito.when(userDataBase.getUserByLoginAndPassword(Matchers.anyString(),Matchers.anyString())).thenAnswer(new Answer<User>() {
+
+            @Override
+            public User answer(InvocationOnMock invocationOnMock) throws Throwable {
+
+                String name = invocationOnMock.getArguments()[0].toString();
+                String surname = invocationOnMock.getArguments()[1].toString();
+
+                for (User user : contentOfDatabase) {
+                    if (user.getName().equals(name) && user.getSurname().equals(surname))
+                        return user;
+                }
+                return null;
+            }
+        });
+
+
+        Encoder encoder = Mockito.mock(Encoder.class);
+
+        Mockito.when(encoder.encode(Matchers.anyString())).thenAnswer(new Answer<String>() {
+            @Override
+            public String answer(InvocationOnMock invocationOnMock) throws Throwable {
+//                do kodowania zosalo uzyte kodowanie Base64 ktore jest juz zaimplementowane w Javie
+                String text = invocationOnMock.getArguments()[0].toString();
+                return encodeBase64(text);
+            }
+        });
         Assert.assertNull(userDataBase.getUserByLoginAndPassword("Fake", new String(Base64.getEncoder().encode("Fake".getBytes()))));
     }
 
@@ -154,6 +287,18 @@ public class LoginServiceTest {
 
     @Test
     public void encoderTest() {
+
+        Encoder encoder = Mockito.mock(Encoder.class);
+
+        Mockito.when(encoder.encode(Matchers.anyString())).thenAnswer(new Answer<String>() {
+            @Override
+            public String answer(InvocationOnMock invocationOnMock) throws Throwable {
+//                do kodowania zosalo uzyte kodowanie Base64 ktore jest juz zaimplementowane w Javie
+                String text = invocationOnMock.getArguments()[0].toString();
+                return encodeBase64(text);
+            }
+        });
+
         String text = "any text to encode";
         Assert.assertEquals(new String(Base64.getEncoder().encode(text.getBytes())), encoder.encode(text));
     }
